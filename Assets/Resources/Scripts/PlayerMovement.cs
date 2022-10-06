db0e5dc4456e500704756b8f400d1f468f667d16
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-10-06 19:44:10 -0300

    Added player movement on the horizontal axis

--- Assets/Resources/Scripts/PlayerMovement.cs
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -6,21 +6,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float runSpeed = 10f;
+
     Vector2 moveInput;
+    Rigidbody2D playerRigidbody;
 
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
         
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);        
+    }
+
+    private void Run()
+    {
+        //Move just on the horizontal axis(x), and ignore the y value.
+        //Acelerates the speed by the runSpeed factor. (Otherwise it would be too slow)
+        //Makes gravity behaves normally on the y, instead of floating in the air
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, playerRigidbody.velocity.y);
+        playerRigidbody.velocity = playerVelocity;
     }
 }
