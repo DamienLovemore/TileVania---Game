0079ddff7281d2097f4af002d536248b551fc368
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-10-07 08:26:48 -0300

    Added player animation swap between idle and running to function

--- Assets/Resources/Scripts/PlayerMovement.cs
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -8,12 +8,14 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float runSpeed = 10f;
 
-    Vector2 moveInput;
-    Rigidbody2D playerRigidbody;
+    private Vector2 moveInput;
+    private Rigidbody2D playerRigidbody;
+    private Animator playerAnimator;
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
         
     void Update()
@@ -27,6 +29,20 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    //Returns true if the player is moving.(Pressing Left or Right Arrows)
+    private bool isPlayerMoving()
+    {
+        //Checks if the player is moving (if it is a value greater than zero,
+        //not taking into account the sign of it)
+        //Uses epsilon cause unity generated float values will never be
+        //exactly zero, even if they are really low.(Epsilon is smallest
+        //possible value for zero)
+        bool playerHasMovement;
+        playerHasMovement = Mathf.Abs(moveInput.x) > Mathf.Epsilon;
+
+        return playerHasMovement;
+    }
+
     //Makes the player moves horizontally
     private void Run()
     {
@@ -35,20 +51,17 @@ public class PlayerMovement : MonoBehaviour
         //Makes gravity behaves normally on the y, instead of floating in the air
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, playerRigidbody.velocity.y);
         playerRigidbody.velocity = playerVelocity;
+
+        //Sets the animation trigger, to be played when moving and stop
+        //when its not supposed to be moving
+        playerAnimator.SetBool("isRunning", isPlayerMoving());
     }
 
     //Makes the player faces the direction he is walking into
     //(left or right)
     private void FlipSprite()
     {
-        //Checks if the player is moving (if it is a value greater than zero,
-        //not taking into account the sign of it)
-        //Uses epsilon cause unity generated float values will never be
-        //exactly zero, even if they are really low.(Epsilon is smallest
-        //possible value for zero)
-        bool playerHasMovement = Mathf.Abs(moveInput.x) > Mathf.Epsilon;
-
-        if (playerHasMovement)
+        if (isPlayerMoving())
         {
             //Math.Sign returns 1 if it is zero or positive, -1 otherwise
             transform.localScale = new Vector2(Mathf.Sign(playerRigidbody.velocity.x), 1f);
