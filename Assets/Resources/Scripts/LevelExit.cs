6bcffd873cdf287b854ea731ac8b2d7b33ef0800
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-10-10 09:42:42 -0300

    Added levels exit, and level design were enhanced

---
+++ Assets/Resources/Scripts/LevelExit.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] private float waitTime = 1f;
+
+    //If something with a collider entered it
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine("LevelComplete");
+        }
+    }
+
+    //Load the next level of the game
+    IEnumerator LevelComplete()
+    {
+        //Waits for the specified amount of seconds
+        yield return new WaitForSecondsRealtime(waitTime);
+        //Load the next level
+        Scene actualScene = SceneManager.GetActiveScene();
+        int levelNumber = actualScene.buildIndex;
+
+        //If it is not on the final level load the next level
+        if (levelNumber != SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(levelNumber + 1);
+        }
+        //If it is just close the game
+        else
+        {
+            Application.Quit();
+        }       
+    }
+}
